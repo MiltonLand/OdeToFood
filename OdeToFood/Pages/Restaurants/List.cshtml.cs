69b1b50f558dc81b4c3330f0d6dd7c262596b4a9
Milton Landriel <milton_landriel@hotmail.com>
2021-01-16 09:46:36 -0300

    a

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood1.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        public IConfiguration Config { get; }
+
+        public ListModel(IConfiguration config)
+        {
+            Config = config;
+        }
+
+        public void OnGet()
+        {
+            Message = Config["Message"];
+        }
+    }
+}
